56775cf4e4e0cdc52ac8024e08de6738e8ba62e6
StanleyN0vak <darkmarek2@gmail.com>
2023-12-06 14:42:06 +0100

    Done Lab7

---
+++ lab7/cfg.php
@@ -0,0 +1,17 @@
+<?php
+    $dbhost = 'localhost';
+    $dbuser = 'root';
+    $dbpass = '';
+    $baza = 'moja_strona';
+    $login = "admin";
+    $pass = "admin123";
+
+    $link = mysqli_connect($dbhost, $dbuser, $dbpass, $baza);
+
+    if (!$link) {
+        die('<b>Przerwane połączenie</b>: ' . mysqli_connect_error());
+    }
+
+    //echo 'Połączenie udane!';
+    mysqli_close($link);
+?>
